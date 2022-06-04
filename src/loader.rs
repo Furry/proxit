770974ba29713dab7d5b39c40dbca0d04de6ba5a
Furry <ender@thefluegges.com>
2022-06-04 16:56:16 -0400

    Removed Depricated _load

--- src/loader.rs
+++ src/loader.rs
@@ -5,36 +5,6 @@ use colored::*;
 use tokio::task::{JoinHandle, spawn_blocking};
 use crate::{logger::LOGGER, lua::bindings};
 
-pub fn _load(_lua: &rlua::Lua) -> Vec<String> {
-    let mut addrs: Vec<String> =  Vec::new();
-    // let mut handles: Vec<JoinHandle<()>> = Vec::new();
-
-    let lua = rlua::Lua::new();
-    // Iterate over every lua file in ./sources
-    for entry in fs::read_dir("./sources").unwrap() {
-        let entry = entry.unwrap();
-        let path = entry.path();
-        let filename = path.file_name().unwrap().to_str().unwrap();
-        if filename.ends_with(".lua") {
-            let contents = fs::read_to_string(path).unwrap();
-            lua.context(|ctx| {
-                let r = ctx.load(&contents).eval::<Table>().unwrap();
-                let x: Vec<String> = r.get("addresses").unwrap();
-                let name: String = r.get("name").unwrap();
-                // rwhandle.lock().unwrap().extend(x);
-                LOGGER.log(format!(
-                    "Loaded {} proxies from {}",
-                    x.len().to_string().blue(),
-                    name.blue()
-                ));
-
-                addrs.extend(x);
-            });
-        }
-    }
-
-    return addrs;
-}
 
 pub async fn load() -> Vec<String> {
     let mut handles: Vec<JoinHandle<Vec<String>>> = Vec::new();
